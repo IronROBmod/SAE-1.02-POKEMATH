c0dfb05def54ebb71eacd410fc61b665369176b1
theo <limule45+github@protonmail.com>
2023-01-03 09:26:26 +0100

    archive zip alpha

---
+++ Pokemath version alpha/Pokemon.java
@@ -0,0 +1,13 @@
+public class Pokemon{
+    String name;
+    int niveau;
+    int pv;
+    int attack;
+    int attackSpe;
+    int defense;
+    int defenseSpe;
+    int vitesse;
+    Element type1;
+    Element type2;
+    Move[] attaques;
+}
